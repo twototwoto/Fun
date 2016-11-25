a042cb0148ffa7b4cd794442d3aa30eff72d238d
ITCoderWang <15076509228@sina.cn>
2016-11-25 12:26:06 +0800

    1.简单地写了一下注册和登录的提示框的leftView的图片处理 2.密码框的密文输入的样式 3.没有对代码做什么优化，写着写着发现想优化的太多，后来一想重点在leftView的显示，就不改了。

--- WYWRegisterLoginTextField/WYWRegisterLoginTextField/ViewController.m
+++ WYWRegisterLoginTextField/WYWRegisterLoginTextField/ViewController.m
@@ -16,7 +16,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CGFloat margin = 20;
+    CGFloat width = self.view.bounds.size.width - margin * 2;
+    CGFloat height = self.view.bounds.size.height * 0.06;
+    CGFloat nameY = margin * 4;
+    CGFloat pwdY = margin * 6.8;
+    
+    UITextField *nameTextField = [[UITextField alloc]initWithFrame:CGRectMake(margin, nameY, width, height)];
+    UITextField *passwdTextField = [[UITextField alloc]initWithFrame:CGRectMake(margin, pwdY, width, height)];
+    
+    nameTextField.placeholder = @"请输入用户名";
+    //一键清空TextField
+    nameTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    
+    UIImageView *imageViewUser = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 30, 20)];
+    imageViewUser.image = [UIImage imageNamed:@"Contacts"];
+    
+    nameTextField.leftView = imageViewUser;
+    //开启leftView总是显示的模式
+    nameTextField.leftViewMode=UITextFieldViewModeAlways;
+    nameTextField.borderStyle = UITextBorderStyleRoundedRect;
+    nameTextField.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    
+    passwdTextField.placeholder = @"请输入密码";
+    //密码的输入的效果
+    passwdTextField.secureTextEntry = YES;
+    //一键清空TextField
+    passwdTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    
+    UIImageView *imageViewPwd=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 30, 20)];
+    imageViewPwd.image=[UIImage imageNamed:@"lock"];
+    
+    passwdTextField.leftView=imageViewPwd;
+    //开启leftView总是显示的模式
+    passwdTextField.leftViewMode=UITextFieldViewModeAlways;
+    passwdTextField.borderStyle = UITextBorderStyleRoundedRect;
+    passwdTextField.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    [self.view addSubview:nameTextField];
+    [self.view addSubview:passwdTextField];
+
 }
 
 
